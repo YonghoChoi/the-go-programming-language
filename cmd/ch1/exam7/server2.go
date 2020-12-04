69846b95212b019aef76fcdec546ccb5afea1cf3
YonghoChoi <yongho1037@gmail.com>
2020-12-05 00:42:04 +0900

    Chapter 8번 예제(웹 서버 접속 시 카운팅)

---
+++ cmd/ch1/exam7/server2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+// go run server2.go
+// 웹 서버 접속 시 카운팅
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	count++
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	fmt.Fprintf(w, "Count %d\n", count)
+}
